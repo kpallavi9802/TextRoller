1f1c8c675a99f98b279e2aaa8c87c12340256a55
Pallavi Kumari <55894301+kpallavi9802@users.noreply.github.com>
2020-08-17 16:21:02 +0530

    Security Concerned

--- PlayWithText/settings.py
+++ PlayWithText/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ixtxbz$y35^j3635bzn@t^a9s0)tjykp087j1tp)zbazck%$v1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
